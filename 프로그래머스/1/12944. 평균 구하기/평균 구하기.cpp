edd84631332516c9166aa8b659f42a1c1dc928b9
bubblebubble43 <songhw1995@gmail.com>
2025-01-02 15:22:12 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.27 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -5,12 +5,9 @@ using namespace std;
 
 double solution(vector<int> arr) {
     double sum = 0;
-    for (int i = 0; i < arr.size(); i++ ){
-        sum += arr[i];
+    for (auto it : arr){
+        sum += it;
     }
-    double avg = sum / arr.size();
-    if (avg == static_cast<int>(avg)){
-        return static_cast<int>(avg);
-    }
-    return avg;
+    
+    return sum / arr.size();
 }
